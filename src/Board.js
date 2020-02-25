d6f2c19f63bb4d83e386a88aecbdcb75c98c83b2
Ronald <ronald.lou88@gmail.com>
2020-02-25 12:29:47 -0600

    added win logic

--- src/Board.js
+++ src/Board.js
@@ -84,14 +84,17 @@ class Board extends Component {
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
-    let hasWon = false;
+    let hasWon = board.every(row => row.every(cell => !cell));
 
-    this.setState({ board, hasWon });
+    this.setState({ board: board, hasWon: hasWon });
   }
 
   /** Render game board or winning message. */
 
   render() {
+    if (this.state.hasWon) {
+      return <h1>YOU WON!!</h1>;
+    }
     // if the game is won, just show a winning msg & render nothing else
 
     // TODO
